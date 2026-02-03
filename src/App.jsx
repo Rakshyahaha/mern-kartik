5d0a1edb62e54775c018f3628e67d0244dff8b9b
Rakshyahaha <168652422+Rakshyahaha@users.noreply.github.com>
2026-02-03 20:09:36 +0545

    created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-red-400" >hello jee</h1>
+      <p>kkkkkk</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Dolorum, doloribus.</p>
+    </div>
+  )
+}
